17f01bd8fa06c5e116eb45a03a1cdc1001876e25
Jorge Neira <jorge.n@mgrouplcs.com>
2017-03-17 23:55:56 -0500

    Initial codebase

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const elixir     = require('laravel-elixir');
+const rupture = require('rupture');
+const postStylus = require('poststylus');
+
+elixir.config.publicPath = 'entrega1';
+elixir.config.sourcemaps = false;
+
+// require('laravel-elixir-vue');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+elixir(mix => {
+
+        mix.stylus('app.styl', elixir.config.publicPath+'/css/app.css', {
+            use: [postStylus(['lost']), rupture()]
+        })
+
+        .browserSync({
+
+            proxy: 'grafica.dev'
+
+        });
+});
